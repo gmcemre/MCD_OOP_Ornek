d91e8a7fcc4379a3139ab6a7f3cd689cbf84ec70
emre gemici <gmcc.emree@gmail.com>
2021-11-29 16:30:13 +0300

    code changed

--- MCD_OOP_Ornek/Classlar/Musteri.cs
+++ MCD_OOP_Ornek/Classlar/Musteri.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MCD_OOP_Ornek.Classlar
+namespace MCD_OOP_Ornek
 {
     class Musteri
     {
--- MCD_OOP_Ornek/Program.cs
+++ MCD_OOP_Ornek/Program.cs
@@ -10,6 +10,67 @@ namespace MCD_OOP_Ornek
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Urun u1 = new Urun()
+                {
+                    UrunAdi ="Ekmek",
+                    UrunId =1000,
+                    UrunKategorisi =UrunKategorileri.Gıda 
+                };
+
+                Urun u2 = new Urun()
+                {
+                    UrunAdi = "Şarj Aleti",
+                    UrunId = 1001,
+                    UrunKategorisi = UrunKategorileri.Elektronik
+                };
+
+                Urun u3 = new Urun()
+                {
+                    UrunAdi = "Süt",
+                    UrunId = 1002,
+                    UrunKategorisi = UrunKategorileri.Gıda
+                };
+
+                Urun u4 = new Urun()
+                {
+                    UrunAdi = "Şampuan",
+                    UrunId = 1003,
+                    UrunKategorisi = UrunKategorileri.Temizlik
+                };
+
+                Musteri m = new Musteri()
+                {
+                    MusteriId = 1,
+                    Cinsiyeti =Cinsiyetler .Erkek ,
+                    DogumTarihi=new DateTime (1993,10,22),
+                    MusteriAdi ="Emre",
+                    MusteriSoyadi ="Gemici"
+
+                };
+
+                UrunSepeti musterininSepeti = new UrunSepeti()
+                {
+                    MusteriId =m.MusteriId ,
+                    SepetId =1
+                };
+                musterininSepeti.UrunlerListesi.Add(u1);
+                musterininSepeti.UrunlerListesi.Add(u2);
+                musterininSepeti.UrunlerListesi.Add(u3);
+                musterininSepeti.UrunlerListesi.Add(u4);
+
+                m.MusterininUrunSepeti = musterininSepeti; //Önce sepeti ve sepete eklenecek ürünleri yukarıda aldım sonra  o sepeti müşterime atadım.
+
+                m.MusteriBilgileriYazdır();
+                m.MusterininSepetiniYazdir();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString ());
+            }
+            Console.ReadKey();
         }
     }
 }
